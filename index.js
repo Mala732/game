a353fdde09d8c108dd61127436513adcd6c9f192
Mala732 <malavika030201@gmail.com>
2023-05-27 00:41:12 +0530

    git push

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const canvas=document.getElementById("game");
 const ctx= canvas.getContext("2d"); 
 
 const GAME_SPEED_START=0.75;
-const GAME_SPEED_INCREMENT= 0.00001;
+const GAME_SPEED_INCREMENT= 0.000017;
 
 const GAME_WIDTH= 800;
 const GAME_HEIGHT =200;
